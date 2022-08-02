dc2acf92a5f91f7d594cf227e92ecbb01f5e94f1
Alex Gravely <tcbskater@hotmail.com>
2022-08-02 18:05:37 -0400

    Make login path context safe

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,10 +1,12 @@
 import type { RequestHandler } from "@sveltejs/kit";
 
-export const GET: RequestHandler = async () => {
+export const GET: RequestHandler = async ({ url }) => {
+    const redirectUri = encodeURIComponent(`${url.origin}/api/auth/discord`);
+
     return {
         status: 302,
         headers: {
-            location: `https://discord.com/api/oauth2/authorize?client_id=1004051425824886947&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fapi%2Fauth%2Fdiscord&response_type=code&scope=email%20identify%20guilds.join`
+            location: `https://discord.com/api/oauth2/authorize?client_id=1004051425824886947&redirect_uri=${redirectUri}&response_type=code&scope=email%20identify%20guilds.join`
         }
     }
 }
