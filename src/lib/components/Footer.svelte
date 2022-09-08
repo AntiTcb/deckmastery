cabf9fee6ceee7a084e0cb2680667afaa4c54bf8
Alex Gravely <tcbskater@hotmail.com>
2022-09-08 15:22:53 -0400

    Improve header and footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,14 +1,21 @@
+<script lang="ts">
+</script>
 <div>
     <p>Yu-Gi-Oh! card images, attributes, and properties used on this site are copyright 4K Media Inc, subsidiary of KONAMI Digital Entertainment, Inc. This website is not created, endorsed, supported, or affiliated with 4k Media or KONAMI.</p>
     <p>All other content, &copy; 2022 - {new Date().getFullYear()} <a href="https://ygorganization.com" target="_blank">The Organization</a></p>
+    <p>Twitter | Discord | About Us | Contact</p>
 </div>
 
 <style>
     div {
         position: absolute;
         bottom: 0;
-        margin: 1rem;
+        padding: 0.5rem 1rem 0.25rem 1rem;
         font-size: 10pt;
+        text-align: center;
+        border-top: 4px solid black;
+        background-color: rgb(40, 40, 40);
+        width: 100%;
     }
     p {
         font-size: 10pt;
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
     import { signOut, getSession } from 'lucia-sveltekit/client'
-
-    import { Header, HeaderNav, HeaderNavItem, HeaderUtilities, HeaderAction, HeaderPanelLinks, HeaderPanelLink,
-        SideNav, SideNavItems, SideNavLink, SkipToContent,
+    import {
+        breakpointObserver,
+        Header, HeaderNav, HeaderNavItem, HeaderNavMenu, HeaderUtilities, HeaderAction, HeaderPanelLinks, HeaderPanelLink,
+        SideNav, SideNavMenu, SideNavMenuItem, SideNavItems, SideNavLink,
     } from "carbon-components-svelte";
+    import { page } from '$app/stores';
+
     import UserProfile from 'carbon-icons-svelte/lib/UserProfile.svelte';
     import UserProfileAlt from 'carbon-icons-svelte/lib/UserProfile.svelte';
 
     const lucia = getSession();
+    let size = breakpointObserver();
 
+    let showSideNav = size.smallerThan("lg");
     let accountPanelIsOpen = false
     let isSideNavOpen = false;
 
     const signOutUser = async () => {
         try {
             await signOut($lucia!.access_token);
-            window.location.href = '/'
+            window.location.href = $page.url.pathname;
         }
         catch {
             console.log('Error signing out')
@@ -23,33 +28,45 @@
     }
 </script>
 
-<Header href="/" persistentHamburgerMenu={true} platformName="Deck Mastery" bind:isSideNavOpen>
-  <svelte:fragment slot="skip-to-content">
-    <SkipToContent />
-  </svelte:fragment>
+<Header href="/" persistentHamburgerMenu={$showSideNav} platformName="Deck Mastery" bind:isSideNavOpen>
   <HeaderNav>
-    <HeaderNavItem href="/combos" text="Combos" />
+    <HeaderNavMenu text="Combos">
+        <HeaderNavItem href="/combos" text="View All Combos" />
+        <HeaderNavItem href="/combos/create" text="Create a New Combo" />
+    </HeaderNavMenu>
     <HeaderNavItem href="/replays" text="Replays" />
+    {#if $lucia?.user?.Role === 'editor' || $lucia?.user?.Role === 'admin'}
+        <HeaderNavItem text="Admin" href="/admin" />
+    {/if}
   </HeaderNav>
   <HeaderUtilities>
     <HeaderAction bind:isOpen={accountPanelIsOpen} icon={UserProfile} closeIcon={UserProfileAlt}>
         {#if $lucia}
             <HeaderPanelLinks>
                 <HeaderPanelLink href="/profile">Hi {$lucia.user.username}</HeaderPanelLink>
+                <HeaderPanelLink href="/profile/combos">My Combos</HeaderPanelLink>
                 <HeaderPanelLink on:click={() => signOutUser()}>Logout</HeaderPanelLink>
             </HeaderPanelLinks>
         {:else}
             <HeaderPanelLinks>
-                <HeaderPanelLink href="/login">Log In</HeaderPanelLink>
+                <HeaderPanelLink href="/login?u={$page.url.pathname}">Log In</HeaderPanelLink>
             </HeaderPanelLinks>
         {/if}
     </HeaderAction>
 </HeaderUtilities>
 </Header>
 
-<SideNav bind:isOpen={isSideNavOpen}>
-  <SideNavItems>
-    <SideNavLink text="Combos" href="/combos" />
-    <SideNavLink text="Replays" href="/replays" />
-  </SideNavItems>
-</SideNav>
+{#if $showSideNav}
+    <SideNav bind:isOpen={isSideNavOpen}>
+        <SideNavItems>
+        <SideNavMenu text="Combos">
+            <SideNavMenuItem href="/combos" text="View All Combos" />
+            <SideNavMenuItem href="/combos/create" text="Create a New Combo" />
+        </SideNavMenu>
+        <SideNavLink text="Replays" href="/replays" />
+        {#if $lucia?.user?.Role === 'editor' || $lucia?.user?.Role === 'admin'}
+            <SideNavLink text="Admin" href="/admin" />
+        {/if}
+        </SideNavItems>
+    </SideNav>
+{/if}
